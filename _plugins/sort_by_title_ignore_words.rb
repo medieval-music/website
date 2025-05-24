a797315c96d8529fef8f74c3ad3e1531a67984e1
ericw9079 <eric@ericw.tech>
2025-05-24 17:31:28 -0400

    Set Up Jekyll

---
+++ _plugins/sort_by_title_ignore_words.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  module SortByTitleIgnoreWords
+    def sort_by_title_ignore_words(input, site)
+      # Load the ignore words from the site's data directory
+      ignore_words = site.data['ignore_words'] || []
+
+      input.sort_by do |item|
+        # Extract the title to sort by
+        title = item.data['title'].to_s.downcase.strip
+
+        # Remove punctuation from the title (ignores leading punctuation and spaces)
+        title = title.gsub(/^\p{P}+/,'') # Remove punctuation from the start
+        title = title.gsub(/[[:punct:]]+$/, '') # Remove punctuation from the end
+
+        # Remove the leading word if it's in the ignore_words list
+        ignore_words.each do |word|
+          # Check if the title starts with the ignored word
+          if title.start_with?("#{word} ")
+            title = title.sub(/^#{word} /, '')
+            break
+          end
+        end
+
+        title
+      end
+    end
+  end
+end
+
+# Register the filter with Liquid
+Liquid::Template.register_filter(Jekyll::SortByTitleIgnoreWords)
